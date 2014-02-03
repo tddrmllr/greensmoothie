51decc52a89c2ed43d27f1890af12c0eab4ec6b2
violentj <miller.todd@live.com>
2014-02-03 13:20:37 -0700

    decompiled

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,6 @@ Bundler.require(:default, Rails.env)
 module Greensmoothie
   class Application < Rails::Application
     config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
-    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
