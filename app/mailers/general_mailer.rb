a22b52159a19373685c280816382327a0bdc6499
violentj <miller.todd@live.com>
2014-07-11 10:40:59 -0600

    fixed contact email

--- app/mailers/general_mailer.rb
+++ app/mailers/general_mailer.rb
@@ -2,6 +2,7 @@ class GeneralMailer < ActionMailer::Base
 
   def support(email, message)
     @message = message
+    @email = email
     mail(:to => 'support@greensmoothie.me', subject: 'Green Smoothie Contact Form', from: email)
   end
 end
