9a97ff706fd67016afc776e4f82cd273f08da816
Todd Miller <todd@miller.im>
2015-12-31 17:21:00 -0700

    post test

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,7 +4,6 @@ class Post < ActiveRecord::Base
   belongs_to :user
 
   default_scope -> { order('created_at DESC') }
-  scope :core,-> { where(core_content: true) }
 
   validates :abstract, presence: true
   validates :body, presence: true
@@ -13,10 +12,6 @@ class Post < ActiveRecord::Base
   validates_length_of :abstract, :minimum => 50, :maximum => 400, :allow_blank => true
 
   before_save :set_url_name
-
-  def dashed_name
-    self.name.downcase.gsub(/[^0-9a-z ]/i, '').gsub(" ", "-")
-  end
 
   def named_route
     "/blog/#{url_name}"
--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
-class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+class PostTest < UnitTest
+  attr_accessor :post
+
+  setup do
+    @post = posts(:admin_post)
+  end
+
+  test 'named_route' do
+    assert_equal '/blog/about-green-smoothies', post.named_route
+  end
+
+  test 'user' do
+    assert_kind_of User, post.user
+  end
+
+  test 'sets url_name on save' do
+    post.update_attributes(name: 'Green Smoothie Tools')
+    assert_equal 'green-smoothie-tools', post.url_name
+  end
 end
