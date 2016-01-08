3504ff25c2bde7ae1dc0fdba10b46ae3992307c6
Todd Miller <todd@miller.im>
2016-01-08 00:59:49 -0700

    better ingredient image

--- app/helpers/images_helper.rb
+++ app/helpers/images_helper.rb
@@ -1,5 +1,19 @@
 module ImagesHelper
-  def media_object_url(resource)
+  def gradient_image(resource)
+    content_tag :div, class: 'show-image', style: "background-image: url(#{large_image_path(resource)})" do
+      image_tag 'white-transparent-gradient.png'
+    end
+  end
+
+  def large_image_path(resource)
+    if resource.image?
+      resource.image.image.url(:large)
+    else
+      asset_path "#{resource.class.name.downcase}.png"
+    end
+  end
+
+  def thumbnail_image_path(resource)
     if resource.image?
       resource.image.image.url(:thumb)
     else
